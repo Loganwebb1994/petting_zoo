191029dacf415b07d09b90dd6824187bed769cf3
Loganwebb1994 <loganwebb1994@gmail.com>
2021-04-22 13:51:49 -0500

    added animal template for 15

---
+++ animals.py
@@ -0,0 +1,134 @@
+from datetime import date
+
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+
+class Bison:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+class Llama:
+    def __init__(self, name, species):
+      self.name = name
+      self.species = species
+      self.date_added = date.today()
+      self.walking = True 
+      
+miss_fuzz = Llama('Miss Fuzz', 'Llama')
+
+
+print(miss_fuzz.name)
